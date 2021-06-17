52bd222047a9bb4cc8720ea767d3d68e2a41a04f
Katie Young <Katie.f.Young@me.com>
2021-06-17 13:11:26 -0400

    Logout route complete

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -27,5 +27,16 @@ router.post(
       });
     }),
   );
-  
+
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+
+
+
 module.exports = router;
