ae59633baa9b7e323e38f0c6630be4b534dede84
Katie Young <Katie.f.Young@me.com>
2021-06-17 17:39:22 -0400

    Signup form done!

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { restoreUser } from './store/session';
 
 const store = configureStore();
 
@@ -16,6 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
+  window.restoreUser = restoreUser;
 }
 
 function Root() {
