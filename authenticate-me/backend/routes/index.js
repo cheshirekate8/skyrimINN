804234e7cb87700e0d9eee05c7514ddac85224db
Katie Young <Katie.f.Young@me.com>
2021-06-17 12:04:11 -0400

    Phase 1 done!

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,9 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+
+
+router.use('/api', apiRouter);
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+
 
 module.exports = router;
