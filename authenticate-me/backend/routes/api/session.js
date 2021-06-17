cf8fe9db7be3a82e1e1523d1bcf8ebb6b2a3306e
Katie Young <Katie.f.Young@me.com>
2021-06-17 13:15:06 -0400

    Signup route complete

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+
+const router = express.Router();
 
 router.post(
     '/',
